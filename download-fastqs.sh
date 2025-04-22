cae7e289289d2df486856ec5ca5c023d6dd1045d
peter-howell <44275653+peter-howell@users.noreply.github.com>
2025-04-22 18:18:12 -0400

    write code to download the fastq files

--- download-fastqs.sh
+++ download-fastqs.sh
@@ -8,6 +8,15 @@ echo "using accessions listed in $filename"
 module load sratoolkit
 
 
-module load parallel
+while IFS= read -r sra_id; do
+	echo "Starting on accession $sra_id"
+	prefetch -p "$sra_id"
+	echo "done prefetching $sra_id. beginning fasterq-dump"
+	fasterq-dump -O data/fastq -e 2 "$sra_id" 
 
+	echo "completed $sra_id"
+done < "$filename"
+
+
+echo "all done"
 
