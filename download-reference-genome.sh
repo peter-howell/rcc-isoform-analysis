34416c47842acb21d8d667b01dde2ea5a103b1fd
peter-howell <44275653+peter-howell@users.noreply.github.com>
2025-04-24 11:03:10 -0400

    use primary genome assembly instead of all

--- download-reference-genome.sh
+++ download-reference-genome.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
 
 
-curl https://ftp.ebi.ac.uk/pub/databases/gencode/Gencode_human/release_47/GRCh38.p14.genome.fa.gz -o GRCh38.p14.genome.fa.gz
-
+curl https://ftp.ebi.ac.uk/pub/databases/gencode/Gencode_human/release_47/GRCh38.primary_assembly.genome.fa.gz -o data/ref/GRCh38.primary_assembly.genome.fa.gz
 
 
